eda160d36cdb72bc3f901373249e23bd1f142563
isaka1022 <isaka1022@gmail.com>
2019-12-14 16:42:11 +0900

    トップページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'top#index'
   resources :questions, only: [:index, :show, :create, :destroy]
   resources :answers, only: [:index, :show, :create, :destroy]
 end
