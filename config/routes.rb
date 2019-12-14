5c5349b7bc77a5fb6c9106ae45cd745b48d0157a
isaka1022 <isaka1022@gmail.com>
2019-12-15 00:04:31 +0900

    プロフィール編集機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users, controllers: {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'top#index'
+  resources :users, only: [:show, :edit, :update]
   resources :questions, only: [:index, :show, :new, :create, :destroy]
   resources :answers, only: [:index, :show, :new, :create, :destroy]
 end
