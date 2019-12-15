7579ccc3b917c7e2dcbbb9bf60ad1e598ae5bc6d
isaka1022 <isaka1022@gmail.com>
2019-12-15 20:49:03 +0900

    MVP完成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  def user_params
+    params.require(:user).permit(:family_name, :family_name_kana, :first_name, :first_name_kana, :nickname, :sex, :faculty, :subject, :course, :laboratory, :club)
+  end
+ 
+end
