5e2c3a4b2c6e4b22716b37e6072bba3ae65225c6
isaka1022 <isaka1022@gmail.com>
2019-12-21 18:12:06 +0900

    新規登録後の遷移先の変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     '/questions/'
   end
+
+  def after_sign_up_path_for(resource)
+    edit_use_path(current_user.id)
+  end
 end
